7da2ca2ce436d4e43af504175a59bd17985e70a7
gangrel2321 <jspang3@hotmail.com>
2022-03-25 22:41:01 -0400

    added ordered label information

--- SnekData.py
+++ SnekData.py
@@ -13,9 +13,15 @@ class SnekData(Dataset):
     def __init__(self, csv_file, root_dir, transform=None):
         metadata = pd.read_csv(csv_file)
         self.classes = metadata['binomial_name'].unique()
+        self.class_to_idx = self._map_classes(metadata)
+        self.metadata = metadata
         self.root = root_dir
         self.transform = transform
-        self.metadata = metadata
+
+    def _map_classes(self, metadata):
+        ids = list(metadata['class_id'].unique())
+        id_to_idx = {ids[i] : i for i in range(len(ids))}
+        return id_to_idx
 
     def __len__(self):
         return len(self.metadata)
@@ -29,6 +35,7 @@ class SnekData(Dataset):
         image = Image.open(img_path).convert('RGB')
         sample = {
             'image' : image,
+            'label_id' : self.class_to_idx[self.metadata["class_id"].iloc[idx]],
             'class_id' : self.metadata["class_id"].iloc[idx],
             'observation_id' : self.metadata["observation_id"].iloc[idx],
             'endemic' : self.metadata["endemic"].iloc[idx],
